190fa88353ad9744614ebc17c553ced55f23cf0b
yongjunp <jhks542280@naver.com>
2023-08-24 10:00:44 +0900

    third commit

---
+++ src/main/java/com/MovieProject/Dao/AdminDao.java
@@ -0,0 +1,25 @@
+package com.MovieProject.Dao;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+
+import com.MovieProject.Dto.Movie;
+import com.MovieProject.Dto.Theaters;
+
+public interface AdminDao {
+
+	@Select("SELECT NVL(MAX(MVCODE),'MV00000') FROM MOVIES") 
+	String selectMaxMvCode();
+
+	int insertMovie(Movie movieInfo);
+
+	@Select("SELECT NVL(MAX(THCODE),'TH00000') FROM THEATERS")
+	String selectMaxThcode();
+
+	@Insert("INSERT INTO THEATERS(THCODE, THNAME, THADDR, THTEL, THINFO, THIMG) VALUES(#{thcode},#{thname},#{thaddr},#{thtel},#{thinfo},#{thimg})")
+	int insertThValue(Theaters theaters);
+
+
+	
+
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,18 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	Hello world!  
+</h1>
+	<a href="/getCgvMovieInfo">CGV 영화 정보 수집</a>
+	<br><br>
+	<a href="/getCgvTheaterInfo">CGV 극장 정보 수집</a>
+	<br><br>
+	<a href="/getCgvScheduleInfo">CGV 상영 시간표 수집</a>
+</body>
+</html>
